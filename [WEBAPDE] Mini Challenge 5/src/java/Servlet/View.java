c7f896e78553b4475f6335d7ad678ffed5f23b92
arvention <amc.antioquia@yahoo.com.ph>
2015-03-31 19:16:21 +0800

    [VIEW SERVLET]

--- [WEBAPDE] Mini Challenge 5/src/java/Servlet/View.java
+++ [WEBAPDE] Mini Challenge 5/src/java/Servlet/View.java
@@ -7,6 +7,7 @@ package Servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -70,7 +71,9 @@ public class View extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        //processRequest(request, response);
+        RequestDispatcher view = request.getRequestDispatcher("Edit.jsp");
+        view.forward(request, response);
     }
 
     /**
